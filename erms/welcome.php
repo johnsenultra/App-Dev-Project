469c8cc8b00098705c666c831420b07f901d6581
johnsen12 <johnsenultra446@gmai.com>
2024-04-21 02:57:53 +0800

    edited the h1

--- erms/welcome.php
+++ erms/welcome.php
@@ -64,7 +64,7 @@ else {
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Welcome Back to ERMS !</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Hello!</div>
 
                       <?php
 $empid=$_SESSION['uid'];
