d99b51996bff379469862913bbbb4f0165c6e54f
johnsen12 <johnsenultra446@gmai.com>
2024-04-21 02:56:29 +0800

    initial commit

---
+++ erms/user_leave.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+include('includes/dbconnection.php');
+
+$user_id = $_SESSION['uid'];
+
+// Retrieve leave requests for the logged-in user
+$sql = "SELECT * FROM leave_requests WHERE employee_id = $user_id";
+$result = $con->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Leave Requests</title>
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+<body id="page-top">
+    <div id="wrapper">
+        <?php include_once('includes/sidebar.php')?>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?php include_once('includes/header.php')?>
+                <div class="container mt-5">
+                    <h2>Leave Requests</h2>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Start Date</th>
+                                <th>End Date</th>
+                                <th>Reason</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                    echo "<tr>";
+                                    echo "<td>" . $row['start_date'] . "</td>";
+                                    echo "<td>" . $row['end_date'] . "</td>";
+                                    echo "<td>" . $row['reason'] . "</td>";
+                                    echo "<td>" . ucfirst($row['status']) . "</td>"; // Display status with first letter capitalized
+                                    echo "</tr>";
+                                }
+                            } else {
+                                echo "<tr><td colspan='4'>No leave requests found.</td></tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/sb-admin-2.min.js"></script>
+</body>
+</html>
