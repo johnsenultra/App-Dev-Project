a9a268ab38cf8c7e866cb3819669e2cb4781dcb4
johnsen12 <johnsenultra446@gmai.com>
2024-04-21 03:02:35 +0800

    changes tthe sematic layout and h1 element

--- erms/admin/includes/footer.php
+++ erms/admin/includes/footer.php
@@ -1,7 +1,7 @@
-   <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span> ERMS Project</span>
-          </div>
-        </div>
-      </footer>
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto" style="opacity: 0.5">
+      <div class="copyright text-center my-auto">
+        <span> ERMS Project</span>
+      </div>
+    </div>
+</footer>
