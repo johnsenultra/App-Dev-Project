9964e7c837d99d9262acc965eaaf623cba2ab085
johnsen12 <johnsenultra446@gmai.com>
2024-04-24 21:00:51 +0800

    remove the search icon

--- erms/includes/header.php
+++ erms/includes/header.php
@@ -1,28 +1,8 @@
-<div style="position: 'sticky', top: '0', zIndex: '1'">
+<div style="position: fixed; top: 0; left: 0; right: 0; z-index: 1;">
   <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
-
-      <!-- <li class="nav-item dropdown no-arrow d-sm-none">
-        <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-search fa-fw"></i>
-        </a> -->
-        <!-- Dropdown - Messages -->
-        <!-- <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-          <form class="form-inline mr-auto w-100 navbar-search">
-            <div class="input-group">
-              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                  <i class="fas fa-search fa-sm"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </li> -->
-
 
       <div class="topbar-divider d-none d-sm-block"></div>
 
@@ -30,15 +10,16 @@
       <li class="nav-item dropdown no-arrow">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <?php
-          $empid=$_SESSION['uid'];
-          $ret=mysqli_query($con,"select EmpFname,EmpLname from employeedetail where ID='$empid'");
-          $row=mysqli_fetch_array($ret);
-          $fname=$row['EmpFname'];
-          $lname=$row['EmpLname'];
+          $empid = $_SESSION['uid'];
+          $ret = mysqli_query($con, "SELECT EmpFname, EmpLname, profile_picture FROM employeedetail WHERE ID='$empid'");
+          $row = mysqli_fetch_array($ret);
+          $fname = $row['EmpFname'];
+          $lname = $row['EmpLname'];
+          $profile_picture = $row['profile_picture'];
           ?>
 
           <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $fname." ".$lname; ?></span>
-          <img class="img-profile rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROKY6nTtUMyJ1H5ZFKloiHcpwiYbiwn89aFSWPQGyZvw&s">
+          <img class="img-profile rounded-circle" src="<?php echo $profile_picture; ?>">
         </a>
         <!-- Dropdown - User Information -->
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -52,7 +33,7 @@
           </a>
           
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="logout.php" >
+          <a class="dropdown-item" href="logout.php">
             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
             Logout
           </a>
